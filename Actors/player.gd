e3ae230b8bbfde39d21c5ec340cdd2e6bb36b19c
Oblitiel <oblitiel@gmail.com>
2025-03-20 14:19:04 +0100

    Base del juego

---
+++ Actors/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var maxSpeed = 100
+@export var aceleration = 800
+@export var friction = 1200
+
+func _physics_process(delta):
+	var inputVector = Vector2.ZERO
+	
+	inputVector.x = Input.get_axis("left","right")
+	inputVector.y = Input.get_axis("up","down")
+	
+	inputVector = inputVector.normalized()
+	
+	if inputVector != Vector2.ZERO:
+		
+		#Tratar la velocidad del jugador
+		velocity = velocity.move_toward(inputVector * maxSpeed, aceleration * delta)
+		
+		var clampValue = Vector2(maxSpeed, maxSpeed)
+		velocity = velocity.clamp(-clampValue,clampValue)
+		
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+
+	move_and_slide()
