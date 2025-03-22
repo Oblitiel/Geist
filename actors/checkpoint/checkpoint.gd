64091f868afb2a3f9c9a47375d27dbdc8bc6495c
nere-eren <nerelorenzo18@gmail.com>
2025-03-22 13:32:27 +0100

    Arregle cosillas

--- actors/checkpoint/checkpoint.gd
+++ actors/checkpoint/checkpoint.gd
@@ -1,10 +1,6 @@
 extends Area2D
 class_name Checkpoint
 
-
-func _ready():
-	connect("body_entered", _on_body_entered)
-
 ## Al entrar un cuerpo en el area, si es el jugador cambia su posicion de reaparicion a la del area
 func _on_body_entered(body):
 	if body is Player:
