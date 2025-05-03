fe54ac0567fe3e6ba501b4f15f10ab9c1ca3366b
nere-eren <nerelorenzo18@gmail.com>
2025-05-03 20:47:29 +0200

    sistema de dialogo y traducciones

---
+++ assets/text/dialogs/DialogLine.gd
@@ -0,0 +1,5 @@
+class_name DialogLine extends Resource
+
+@export var name:String
+@export var text:String
+@export var image:CompressedTexture2D
---
+++ ui/DialogScreen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@export var dialogScript: Array[DialogLine]
+var line : int = 0
+
+func  _ready():
+	update(line)
+
+func  _input(event):
+	if event.is_action_pressed("interact"):
+		line += 1
+		if line >= dialogScript.size():
+			queue_free()
+		else:
+			update(line)
+
+func update(index):
+	$TextBox/Text.text = dialogScript[index].text
+	$Character.texture = dialogScript[index].image
+	$TextBox2/Text2.text = dialogScript[index].name
