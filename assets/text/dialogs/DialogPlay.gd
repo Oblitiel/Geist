f679a653fa059ed10a813de0edc9334a9bc617bc
nere-eren <nerelorenzo18@gmail.com>
2025-05-05 14:12:53 +0200

    dialogPlay
    he roto algo

---
+++ assets/text/dialogs/DialogPlay.gd
@@ -0,0 +1,24 @@
+class_name DialogPlay extends Resource
+
+#array con las lineas del dialogo
+@export var dialogScript: Array[DialogLine]
+
+#variables para iterar
+var start: int = 0
+var current: int = 0
+var end: int
+
+#funciones para hacer la clase iterable
+func shouldContinue():
+	return (current < end)
+
+func _iter_init(iter):
+	end = dialogScript.size()
+	return shouldContinue()
+
+func _iter_next(iter):
+	current += 1
+	return shouldContinue()
+
+func _iter_get(iter):
+	return dialogScript[current]
