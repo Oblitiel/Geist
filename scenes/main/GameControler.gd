640af27e04b85d5458b7c69de829aebe92d795bb
Oblitiel <oblitiel@gmail.com>
2025-05-14 17:38:19 +0200

    Changes to work with menus

--- scenes/main/GameControler.gd
+++ scenes/main/GameControler.gd
@@ -15,3 +15,17 @@ func changeScene(newScene: String, delete: bool = true, keepRunning : bool = fal
 	var new = load("res://scenes/levels/" + newScene + ".tscn")
 	add_child(new)
 	currentScene = new
+
+func showOptionsMenu():
+	pauseGame()
+	pass
+
+func showDialogMenu(dialogPlay : DialogPlay):
+	pauseGame()
+	pass
+
+func pauseGame():
+	get_tree().paused = true
+
+func runGame():
+	get_tree().paused = false
