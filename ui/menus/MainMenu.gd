4e1fb273687b52430b895bc29586c6aa8936a3a2
nere-eren <nerelorenzo18@gmail.com>
2025-05-14 17:26:14 +0200

    poniendo el optionsMenu en el MainMenu

--- ui/menus/MainMenu.gd
+++ ui/menus/MainMenu.gd
@@ -1,12 +1,26 @@
 extends Control
 
+var MAINSCENE: String = "res://Levels/level.tscn"
+
 func _ready():
 	$VBoxContainer/PlayButton.pressed.connect(play)
 	$VBoxContainer/ExitButton.pressed.connect(exitGame)
+	$VBoxContainer/OptionsButton.pressed.connect(showOptions)
+	$VBoxContainer/LoadButton.pressed.connect(loadGame)
+	
+func _input(event):
+	if event.is_action_released("ui_cancel"):
+		if $OptionsMenu.visible == true:
+			$OptionsMenu.visible = false
 	
 func play():
-	get_tree().change_scene_to_file("res://Levels/test_level.tscn")
+	get_tree().change_scene_to_file(MAINSCENE)
 
 func exitGame():
 	get_tree().quit()
 	
+func showOptions():
+	$OptionsMenu.visible = true
+
+func loadGame():
+	pass
