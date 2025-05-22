a1ebe61c3d2615292d4b5cf78ef63bdbaf83e7fc
nere-eren <nerelorenzo18@gmail.com>
2025-05-22 22:02:42 +0200

    pequeños arreglillos

--- scenes/actors/npc/npc.gd
+++ scenes/actors/npc/npc.gd
@@ -2,10 +2,13 @@ class_name NPC extends CharacterBody2D
 
 @export var dialog: DialogPlay
 @export var possesionData : BodyProperties
+@export var sprite: Sprite2D
+
+func _ready():
+	$Sprite2D.texture = sprite
 
 func talk():
 	Global.gameControler.showDialog(dialog)
-	print("pene")
 
 func getProperties():
 	return possesionData
