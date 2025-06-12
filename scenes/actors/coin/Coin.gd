8845485ff729d5f31122608be8a98e1af587733c
nere-eren <nerelorenzo18@gmail.com>
2025-06-12 23:37:19 +0200

    sonido monedas

--- scenes/actors/coin/Coin.gd
+++ scenes/actors/coin/Coin.gd
@@ -4,6 +4,10 @@ class_name Coin extends Node
 
 func _on_body_entered(body):
 	if body is Player:
+		$AudioStreamPlayer.play()
+		$Sprite2D.hide()
+		$CollisionShape2D.disabled = true
+		await $AudioStreamPlayer.finished
 		SaveManager.current_game_state.coins.append(idCoin)
 		Global.gameControler.addCoin()
 		queue_free()
