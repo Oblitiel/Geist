6e53f1fd8691892fd79048608a9dae76751b1e12
nere-eren <nerelorenzo18@gmail.com>
2025-06-13 11:08:51 +0200

    Sonido moneda

--- scenes/actors/coin/Coin.gd
+++ scenes/actors/coin/Coin.gd
@@ -4,9 +4,15 @@ class_name Coin extends Node
 
 func _on_body_entered(body):
 	if body is Player:
+<<<<<<< Updated upstream
 		$AudioStreamPlayer.play()
 		$Sprite2D.hide()
 		$CollisionShape2D.disabled = true
+=======
+		$CollisionShape2D.set_deferred("disabled", true)
+		$Sprite2D.hide()
+		$AudioStreamPlayer.play()
+>>>>>>> Stashed changes
 		await $AudioStreamPlayer.finished
 		SaveManager.current_game_state.coins.append(idCoin)
 		Global.gameControler.addCoin()
